8d2696267710b08d81e936eac3869f82bed55ff4
Erlend Vige <erlend.vige@gmail.com>
2017-11-27 11:46:00 +0100

    CSS-modules now working

---
+++ src/app/components/step1/Step1.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import s from './styles.less';
+
+const Step1 = () => (
+    <div className={s.container}>Step1</div>
+);
+
+export default Step1;
--- src/app/util/routing/routes.config.js
+++ src/app/util/routing/routes.config.js
@@ -1,6 +1,9 @@
+import React from 'react';
+
 import EngangsstonadIndex from './../../components/engangsstonad/EngangsstonadIndex';
 import Step1 from './../../components/engangsstonad/Step1';
 import Step2 from './../../components/engangsstonad/Step2';
+import Home from './../../components/home/Home';
 
 const routeConfig = [
     {
@@ -15,6 +18,10 @@ const routeConfig = [
             { path: '/engangsstonad/steg-1', component: Step1 },
             { path: '/engangsstonad/steg-2', component: Step2 }
         ]
+    },
+    {
+        path: '/test',
+        component: () => (<Home />)
     }
 ];
 
