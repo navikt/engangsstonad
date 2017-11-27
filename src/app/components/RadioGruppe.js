7615535a0f1334a66df39dddd7f8d266befdee20
Odd Einar Hoel <h151159@odds-mbp.adeo.no>
2017-11-27 16:14:32 +0100

    radio gruppe

---
+++ src/app/components/RadioGruppe.js
@@ -0,0 +1,41 @@
+// @flow
+import React from 'react';
+import { Radio } from 'nav-frontend-skjema';
+
+export const satsRadioData = [
+    {
+        label: '80%',
+        value: 80
+    },
+    {
+        label: '100%',
+        value: 100
+    }
+];
+
+export const omsorgRadioData = [
+    {
+        label: 'Felles omsorg',
+        value: ''
+    },
+    {
+        label: 'Aleneomsorg',
+        value: ''
+    }
+];
+
+type Props = {
+    listOfRadioData: Object,
+    name: string
+}
+
+const RadioGruppe = (props: Props) => (
+    props.listOfRadioData.map((radioData) => (
+        <Radio
+            {...radioData}
+            key={radioData.value}
+            name={props.name}
+        />
+    ))
+);
+export default RadioGruppe;
