8aa5614e7787ce15ca7612fa3a86193d3897420c
Erlend Vige <erlend.vige@gmail.com>
2017-11-28 17:01:13 +0100

    Remove comments from webpack config

--- src/config/webpack.config.js
+++ src/config/webpack.config.js
@@ -23,55 +23,6 @@ const webpackConfig = {
             },
             lessLoader(/node_modules/, true),
             lessLoader(path.resolve(__dirname, './../app')),
-             /*   test: /\.less$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                modules: true,
-                                importLoaders: 1,
-                                localIdentName: '[name]__[local]__[hash:base64:5]'
-                            }
-                        },
-                        {
-                            loader: 'postcss-loader'
-                        },
-                        {
-                            loader: 'less-loader',
-                            options: {
-                                globalVars: {
-                                    coreModulePath: '\'~\'',
-                                    nodeModulesPath: '\'~\''
-                                }
-                            }
-                        }
-                    ]
-                }),
-                include: path.resolve(__dirname, './../app')
-            },
-            {
-                test: /\.less$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        {
-                            loader: 'css-loader',
-                        },
-                        {
-                            loader: 'less-loader',
-                            options: {
-                                modifyVars: {
-                                    nodeModulesPath: '~',
-                                    coreModulePath: '~'
-                                }
-                            }
-                        }
-                    ]
-                }),
-                include: path.resolve(__dirname, './../../node_modules')
-            },*/
             {
                 test: /\.(png|jpg|gif|svg)$/,
                 use: [
