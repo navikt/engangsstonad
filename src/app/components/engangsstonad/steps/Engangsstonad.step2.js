b2d8e1407db22d8e8aa9691f02a98483e3b9e55b
Erlend Vige <erlend.vige@gmail.com>
2017-11-29 15:46:33 +0100

    Fix warnings

--- src/app/components/engangsstonad/steps/Engangsstonad.step2.js
+++ src/app/components/engangsstonad/steps/Engangsstonad.step2.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Row, Column } from 'nav-frontend-grid';
 
-const Step2 = () => (<h2>Step 2</h2>);
+const Step2 = () => (
+    <div>
+        <p>Step 2</p>
+        <Row>
+            <Column xs="12" />
+        </Row>
+        <Row>
+            <Column xs="12" />
+        </Row>
+    </div>
+);
 
 export default Step2;
