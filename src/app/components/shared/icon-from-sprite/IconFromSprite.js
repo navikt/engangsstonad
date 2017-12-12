25b74e409dfb583665a26616dee771d07186d172
Erlend Vige <erlend.vige@gmail.com>
2017-12-12 09:08:50 +0100

    Fix icon sprite size issues

--- src/app/components/shared/icon-from-sprite/IconFromSprite.js
+++ src/app/components/shared/icon-from-sprite/IconFromSprite.js
@@ -2,20 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class IconFromSprite extends React.Component {
-    renderIcon() {
+    renderIcon(svgAttrs) {
         return (
-            <svg viewBox={this.props.iconRef.viewBox}>
+            <svg {...svgAttrs}>
                 <use xlinkHref={`#${this.props.iconRef.id}`} />
             </svg>
         );
     }
 
     render() {
-        return (this.renderIcon());
+        const svgAttrs = {
+            viewbox: this.props.iconRef.viewBox,
+            height: this.props.height,
+            width: this.props.width
+        };
+
+        return (this.renderIcon(svgAttrs));
     }
 }
 
 IconFromSprite.propTypes = {
     // eslint-disable-next-line react/forbid-prop-types, react/require-default-props
-    iconRef: PropTypes.object
+    iconRef: PropTypes.object,
+    height: PropTypes.number,
+    width: PropTypes.number
+};
+
+IconFromSprite.defaultProps = {
+    height: undefined,
+    width: undefined
 };
