28f99cd47953563fb99ed3a5217f13f1e7667fe6
Andreas Dybdahl <andreas.dybdahl@nav.no>
2017-12-13 13:10:00 +0100

    Overhaul of engangsstonad step 2

--- src/app/components/engangsstonad/steps/Engangsstonad.step2.js
+++ src/app/components/engangsstonad/steps/Engangsstonad.step2.js
@@ -1,17 +1,70 @@
-// @flow
-import React from 'react';
+import React, { Component } from 'react';
 
-import { Normaltekst } from 'nav-frontend-typografi';
+import { Normaltekst, Element } from 'nav-frontend-typografi';
+import { ToggleGruppe, ToggleKnapp } from 'nav-frontend-skjema';
+
 import DialogBox from '../../shared/dialog-box/DialogBox';
+import DateInput from '../../shared/date-input/DateInput';
+import AttchmentList from '../../shared/attachment-list/AttachmentList';
+import AttchmentButton from '../../shared/attachment-button/AttachmentButton';
 
-const Step2 = () => (
-    <div className="engangsstonadStep2">
-        <DialogBox type="info">
-            <Normaltekst>
-                Vi trenger mer informasjon fra deg om barnet eller barna søknaden gjelder
-            </Normaltekst>
-        </DialogBox>
-    </div>
-);
+class Step2 extends Component {
+    constructor(props) {
+        super(props);
+        this.toggleBirth = this.toggleBirth.bind(this);
+
+        this.state = {
+            childBorn: undefined
+        };
+    }
+
+    toggleBirth(e) {
+        if (e.target.value === 'childNotBorn') {
+            this.setState({
+                childBorn: false
+            });
+        } else {
+            this.setState({
+                childBorn: true
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className="engangsstonadStep2">
+                <DialogBox type="info">
+                    <Normaltekst>
+                        Vi trenger mer informasjon fra deg om barnet eller barna søknaden gjelder
+                    </Normaltekst>
+                </DialogBox>
+                <Element>Søknaden gjelder en fødsel som er...</Element>
+                <ToggleGruppe onChange={this.toggleBirth} name="isChildBorn">
+                    <ToggleKnapp value="childBorn">tilbake i tid</ToggleKnapp>
+                    <ToggleKnapp value="childNotBorn">frem i tid</ToggleKnapp>
+                </ToggleGruppe>
+                {this.state.childBorn === false &&
+                    <div>
+                        <Element>og jeg venter...</Element>
+                        <ToggleGruppe onChange={() => undefined} name="noOfChildren">
+                            <ToggleKnapp value="1">et barn</ToggleKnapp>
+                            <ToggleKnapp value="2">tvillinger</ToggleKnapp>
+                            <ToggleKnapp value="3">trillinger</ToggleKnapp>
+                        </ToggleGruppe>
+                        <Element>med termindato den...</Element>
+                        <DateInput label="" />
+                        <DialogBox type="warning">
+                            <Normaltekst>
+                                Siden barnet ikke er født må du legge ved terminbekreftelse fra jordmor eller lege
+                            </Normaltekst>
+                        </DialogBox>
+                        <AttchmentList label="" />
+                        <AttchmentButton />
+                    </div>
+                }
+            </div>
+        );
+    }
+}
 
 export default Step2;
