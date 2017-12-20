c8fa9d374103bd4c7ffdd8ff479044c7bbbb82b8
Andreas Dybdahl <andreas.dybdahl@nav.no>
2017-12-20 14:31:40 +0100

    Forgot to remove props

--- src/app/components/shared/display-text-with-label/DisplayTextWithLabel.js
+++ src/app/components/shared/display-text-with-label/DisplayTextWithLabel.js
@@ -5,12 +5,7 @@ import { EtikettLiten, Normaltekst } from 'nav-frontend-typografi';
 
 import './displayTextWithLabel.less';
 
-type Props = {
-    label: string,
-    text: string
-};
-
-export const DisplayTextWithLabel = (props: Props) => (
+export const DisplayTextWithLabel = (props) => (
     <div className="textWithLabel">
         <EtikettLiten className="textWithLabel__label">{props.label}</EtikettLiten>
         <Normaltekst className="textWithLabel__text">{props.text}</Normaltekst>
