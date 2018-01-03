cb9caee945a96d271c7641c50daea9e51f7580d0
Frode Hansen <frode.hansen@bekk.no>
2018-01-03 12:11:40 +0100

    prettier test

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -1,10 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 function fetchData() {
-    return (axios.get('https://foreldrepenger-selvbetjening-engangsstonad.nais.oera-q.local/rest/personinfo'));
+	return axios.get(
+		"https://foreldrepenger-selvbetjening-engangsstonad.nais.oera-q.local/rest/personinfo"
+	);
 }
 
 const Api = { fetchData };
 
 export default Api;
-
--- src/app/components/engangsstonad/Engangsstonad.index.js
+++ src/app/components/engangsstonad/Engangsstonad.index.js
@@ -1,49 +1,51 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import renderChildRoutes from 'util/routing';
-import StepBasedForm from 'shared/step-based-form/StepBasedForm';
-import { connect } from 'react-redux';
-import { getDataRequested } from './../../redux/ducks/Engangsstonad.duck';
-import './engangsstonad.less';
+import React from "react";
+import PropTypes from "prop-types";
+import renderChildRoutes from "util/routing";
+import StepBasedForm from "shared/step-based-form/StepBasedForm";
+import { connect } from "react-redux";
+import { getDataRequested } from "./../../redux/ducks/Engangsstonad.duck";
+import "./engangsstonad.less";
 
 // eslint-disable-next-line react/prefer-stateless-function
 export class EngangsstonadIndex extends React.Component {
-    componentWillMount() {
-        this.props.dispatch(getDataRequested());
-    }
+	componentWillMount() {
+		this.props.dispatch(getDataRequested());
+	}
 
-    render() {
-        return (
-            <div className="engangsstonad">
-                <StepBasedForm
-                    nextButtonEnabled
-                    routes={renderChildRoutes(this.props.routes) || []}
-                    title="Søknad om engangsstønad"
-                    location={this.props.location}
-                />
-                { this.props.data && <div>Data: { JSON.stringify(this.props.data) }</div> }
-                { this.props.error && <div>Error getting data: { JSON.stringify(this.props.error) }</div> }
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="engangsstonad">
+				<StepBasedForm
+					nextButtonEnabled
+					routes={renderChildRoutes(this.props.routes) || []}
+					title="Søknad om engangsstønad"
+					location={this.props.location}
+				/>
+				{this.props.data && <div>Data: {JSON.stringify(this.props.data)}</div>}
+				{this.props.error && (
+					<div>Error getting data: {JSON.stringify(this.props.error)}</div>
+				)}
+			</div>
+		);
+	}
 }
 
 EngangsstonadIndex.propTypes = {
-    routes: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
-    location: PropTypes.shape({}).isRequired,
-    dispatch: PropTypes.func.isRequired,
-    data: PropTypes.shape({}),
-    error: PropTypes.shape({})
+	routes: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+	location: PropTypes.shape({}).isRequired,
+	dispatch: PropTypes.func.isRequired,
+	data: PropTypes.shape({}),
+	error: PropTypes.shape({})
 };
 
 EngangsstonadIndex.defaultProps = {
-    data: null,
-    error: null
+	data: null,
+	error: null
 };
 
-const mapStateToProps = (state) => ({
-    data: state.engangsstonadReducer.data,
-    error: state.engangsstonadReducer.error
+const mapStateToProps = state => ({
+	data: state.engangsstonadReducer.data,
+	error: state.engangsstonadReducer.error
 });
 
 export default connect(mapStateToProps)(EngangsstonadIndex);
