0fd66d543f3e9464b2815664bf24cdebc21b489c
Erlend Vige <erlend.vige@gmail.com>
2018-01-04 14:45:32 +0100

    Add environment variable for rest api to nais.yml

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 
 const server = express();
 server.use(express.static(path.resolve(__dirname, 'dist')));
+
+console.log('Env:', process.env['foreldrepengesoeknad-api']);
 
 server.get('/engangsstonad/?*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
