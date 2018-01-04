1ad5a3fafbc1e96b36510a46d4d9a1351001b1cb
Erlend Vige <erlend.vige@gmail.com>
2018-01-04 13:57:28 +0100

    Health check URL fix

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ server.get('/engangsstonad/?*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
-server.get('/isAlive', (req, res) => res.sendStatus(200));
-server.get('/isReady', (req, res) => res.sendStatus(200));
+server.get('/health/isAlive', (req, res) => res.sendStatus(200));
+server.get('/health/isReady', (req, res) => res.sendStatus(200));
 
 server.listen(8080, () => {
     console.log('App listening on port 8080');
