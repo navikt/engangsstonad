79b6e73fe3a3dd473fdcd5df10138b3053629d16
Erlend Vige <erlend.vige@gmail.com>
2018-01-11 09:17:48 +0100

    Remove test failing due to Back button being deleted from Stepper component

--- src/app/components/shared/stepper/Stepper.spec.js
+++ src/app/components/shared/stepper/Stepper.spec.js
@@ -7,13 +7,6 @@ describe('<Stepper />', () => {
 		expect(wrapper.find('StepperButton'));
 	});
 
-	it('should render StepperButton with step back properties', () => {
-		const wrapper = shallow(<Stepper previousRoute="/" showStepBack />);
-		const stepperButton = wrapper.find('StepperButton');
-		expect(stepperButton.props()).to.have.property('label', 'Tilbake');
-		expect(stepperButton.props()).to.have.property('href', '/');
-	});
-
 	it('should render StepperButton with step ahead properties', () => {
 		const wrapper = shallow(<Stepper nextRoute="/" showStepAhead />);
 		const stepperButton = wrapper.find('StepperButton');
