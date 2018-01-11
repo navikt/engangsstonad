c8746822b6fb4a6d58548f14103faf97b7b9856f
Frode Hansen <frode.hansen@bekk.no>
2018-01-11 12:08:46 +0100

    HeaderIllustrasjon

---
+++ src/app/components/shared/header-illustrasjon/HeaderIllustrasjon.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+
+import { Sidetittel } from 'nav-frontend-typografi';
+
+import CustomSVG from '../custom-svg/CustomSVG';
+
+import './headerIllustration.less';
+
+import SpeechBubble from '../speech-bubble/SpeechBubble';
+
+const HeaderIllustration = ({ title, dialog, svg }) => (
+	<div className="headerIllustration">
+		<div className="headerIllustration__title">
+			<Sidetittel>{title}</Sidetittel>
+		</div>
+		<div className="headerIllustration__speechBubble">
+			<SpeechBubble title={dialog.title} text={dialog.text} />
+		</div>
+		<div className="headerIllustration__illustration">
+			<CustomSVG iconRef={svg} />
+		</div>
+	</div>
+);
+
+HeaderIllustration.propTypes = {
+	title: PropTypes.string.isRequired,
+	dialog: PropTypes.shape({
+		title: PropTypes.string
+	}),
+	svg: PropTypes.object
+};
+
+HeaderIllustration.defaultProps = {
+	dialog: null,
+	svg: null
+};
+
+export default HeaderIllustration;
