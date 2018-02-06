e6aa5e31a22cbf8d1e12aadd6ebf689d92ca8b55
AndreasDybdahl <adybdahl@one.steria.dom>
2018-02-06 14:30:02 +0100

    Cleanup of unused files

--- src/app/util/routing/index.js
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-
-// import RouteWithSubRoutes from './route.component';
-
-// export default (routeConfig) =>
-// 	routeConfig.map((route) => (
-// 		<RouteWithSubRoutes {...route} key={route.path || '404'} />
-// 	));
