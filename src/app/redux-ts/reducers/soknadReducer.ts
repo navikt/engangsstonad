eff63f00fd7086bab7f9d63762f2db4badda537e
Erlend Vige <erlend.vige@gmail.com>
2018-02-06 16:07:41 +0100

    Implement reducer-logic for Utenlandsopphold

--- src/app/redux-ts/reducers/soknadReducer.ts
+++ src/app/redux-ts/reducers/soknadReducer.ts
@@ -1,11 +1,23 @@
 import { SoknadActionKeys, SoknadActionTypes } from '../actions/soknad/soknadActionDefinitions';
+import { Utenlandsopphold } from "../../types/Medlemsskap";
 
 const getDefaultState = () => ({
-    visits: []
+    utenlandsopphold: [] as Utenlandsopphold[]
 });
 
 const soknadReducer = (state = getDefaultState(), action: SoknadActionTypes) => {
     switch(action.type) {
+        case SoknadActionKeys.ADD_UTENLANDSOPPHOLD:
+            const utenlandsopphold = state.utenlandsopphold;
+            utenlandsopphold.concat(action.utenlandsopphold);
+            return { ...state, utenlandsopphold };
+        case SoknadActionKeys.EDIT_UTENLANDSOPPHOLD:
+            const index = state.utenlandsopphold.indexOf(action.utenlandsopphold);
+            state.utenlandsopphold[index] = action.utenlandsopphold;
+            return { ...state };
+        case SoknadActionKeys.DELETE_UTENLANDSOPPHOLD:
+            state.utenlandsopphold.splice(0, 1, action.utenlandsopphold);
+            return {...state};
         case SoknadActionKeys.SET_ANTALL_BARN:
             return { ...state, antallBarn: action.antallBarn };
         case SoknadActionKeys.SET_FODSELSDATO:
