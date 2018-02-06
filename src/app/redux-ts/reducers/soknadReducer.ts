b5dccf55ac89cef57ac0e67fe26fc08f8775c753
Erlend Vige <erlend.vige@gmail.com>
2018-02-06 15:35:31 +0100

    Start implementation of soknadReducer

---
+++ src/app/redux-ts/reducers/soknadReducer.ts
@@ -0,0 +1,31 @@
+import { SoknadActionKeys, SoknadActionTypes } from '../actions/soknad/soknadActionDefinitions';
+
+const getDefaultState = () => ({
+    activeRoute: null,
+    visits: [],
+    isLoading: false,
+    language: 'nb'
+});
+
+const soknadReducer = (state = getDefaultState(), action: SoknadActionTypes) => {
+    switch(action.type) {
+        case SoknadActionKeys.SET_ANTALL_BARN:
+            return { ...state, antallBarn: action.antallBarn };
+        case SoknadActionKeys.SET_FODSELSDATO:
+            return { ...state, antallBarn: action.fodselsdato };
+        case SoknadActionKeys.SET_TERMIN_DATO:
+            return { ...state, terminDato: action.terminDato };
+        case SoknadActionKeys.SET_UTSTEDT_DATO:
+            return { ...state, utstedtDato: action.utstedtDato };
+        case SoknadActionKeys.SET_ARBEID_SISTE_12:
+            return { ...state, arbeidSiste12: action.arbeidSiste12 };
+        case SoknadActionKeys.SET_FODSEL_I_NORGE:
+            return { ...state, fodselINorge: action.fodselINorge };
+        case SoknadActionKeys.SET_I_NORGE_SISTE_12:
+            return { ...state, iNorgeSiste12: action.iNorgeSiste12 };
+        case SoknadActionKeys.SET_I_NORGE_NESTE_12:
+            return { ...state, iNorgeNeste12: action.iNorgeNeste12 };
+    }
+};
+
+export default soknadReducer;
