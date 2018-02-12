02c8b27199356c9a4306d8f3193fdcfab97f9af9
Erlend Vige <erlend.vige@gmail.com>
2018-02-12 14:55:49 +0100

    Rewrite stepIndicator to ts

---
+++ src/app/components/shared/progress-indicator/StepIndicator.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import Step from './Step';
+const { Ingress } = require('nav-frontend-typografi');
+import 'nav-frontend-stegindikator-style';
+import './stepIndicator.less';
+
+interface Props {
+    stepTitles: string[];
+    activeStep: number;
+}
+
+const StepIndicator: React.StatelessComponent<Props> = ({ stepTitles, activeStep }) => (
+    <div
+        role="progressbar"
+        aria-valuenow={activeStep}
+        aria-valuemin="1"
+        aria-valuemax={stepTitles.length}
+    >
+        <div className="stegindikatortittel">
+            <Ingress>{stepTitles[activeStep - 1]}</Ingress>
+        </div>
+        <ul className="stegindikator">
+            {stepTitles.map((step, index) => (
+                <Step
+                    activeStep={activeStep}
+                    key={step}
+                    title={step}
+                    step={index + 1}
+                />
+            ))}
+        </ul>
+    </div>
+);
+
+export default StepIndicator;
