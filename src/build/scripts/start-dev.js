8dd54f31723dff44b5424cb4eb2a91bbf25f6b7f
AndreasDybdahl <adybdahl@one.steria.dom>
2018-02-23 14:49:58 +0100

    Changed linting to use spaces and not tabs

--- src/build/scripts/start-dev.js
+++ src/build/scripts/start-dev.js
@@ -5,13 +5,13 @@ const configureDevServer = require('../webpack/devserver.config');
 const getDecorator = require('./decorator');
 
 getDecorator().then((decoratorData) => {
-	const compiler = webpack(webpackConfig);
-	const server = new WebpackDevServer(
-		compiler,
-		configureDevServer(decoratorData)
-	);
+    const compiler = webpack(webpackConfig);
+    const server = new WebpackDevServer(
+        compiler,
+        configureDevServer(decoratorData)
+    );
 
-	server.listen(8080, '127.0.0.1', () =>
-		console.log('Started server on http://localhost:8080')
-	);
+    server.listen(8080, '127.0.0.1', () =>
+        console.log('Started server on http://localhost:8080')
+    );
 });
