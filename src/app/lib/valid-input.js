73f5520a42aec3a8176aa756ba575c9881e95b01
Erlend Vige <erlend.vige@gmail.com>
2018-02-27 07:36:52 +0100

    Add form-validation logic from nav-frontend (PR#179)

---
+++ src/app/lib/valid-input.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+import { Input } from '../../nav-frontend-skjema';
+import ValidBase from './';
+
+class ValidInput extends Component { // eslint-disable-line react/prefer-stateless-function
+    render() {
+        const { ...other } = this.props;
+        return (
+            <ValidBase component={Input} {...other} />
+        );
+    }
+}
+
+export default ValidInput;
+/* eslint-enable */
