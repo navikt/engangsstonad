ab4fbbf6830f3a516ba21fb4196221dee5661dd6
Erlend Vige <erlend.vige@gmail.com>
2018-03-05 13:25:45 +0100

    Remove broken refresh
    Fixes issues with over-aggressive watching and page refreshes during development

--- src/build/webpack/webpack.config.dev.js
+++ src/build/webpack/webpack.config.dev.js
@@ -3,10 +3,6 @@ const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpackConfig = require('./webpack.config.global.js');
 
-webpackConfig.entry = webpackConfig.entry.concat([
-	'webpack-dev-server/client?http://localhost:8080'
-]);
-
 webpackConfig.plugins.push(
 	new HtmlWebpackPlugin({
 		template: './src/app/index.html',
