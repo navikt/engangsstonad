0b5e563aed55e2d48eb5820842b410e6cc493c4b
Christian H. Mosveen <mosveen@gmail.com>
2018-03-06 13:44:17 +0100

    Fix cypress tests (and add test for male applicant)

--- cypress/integration/frontpage_spec.js
+++ cypress/integration/frontpage_spec.js
@@ -4,24 +4,24 @@ describe('Frontpage', () => {
         cy.route('GET', '**/personinfo*', 'fixture:users/default.json');
 
         cy.visit('/engangsstonad');
-        cy.contains('Søknad om engangsstønad')
-    })
+        cy.contains('Søknad om engangsstønad');
+    });
 
     it('Should load underage-page for user under 18 years', () => {
         cy.server();
         cy.route('GET', '**/personinfo*', 'fixture:users/under18.json');
 
         cy.visit('/engangsstonad');
-        cy.contains('Søknad om engangsstønad')
-        cy.url().should('contains', '/underAge')
-    })
+        cy.contains('Søknad om engangsstønad');
+        cy.contains('under 18 år') // TODO: Burde bruke en hook (klasse e.l.) i stedet for tekst
+    });
 
-    // it('Should load male-page for male users', () => {
-    //     cy.server();
-    //     cy.route('GET', '**/personinfo*', 'fixture:users/male.json');
-    //
-    //     cy.visit('/engangsstonad');
-    //     cy.contains('Søknad om engangsstønad')
-    //     cy.url().should('contains', '/male')
-    // })
+    it('Should load male-page for male users', () => {
+        cy.server();
+        cy.route('GET', '**/personinfo*', 'fixture:users/male.json');
+
+        cy.visit('/engangsstonad');
+        cy.contains('Søknad om engangsstønad');
+        cy.contains('registrert som mann'); // TODO: Burde bruke en hook (klasse e.l.) i stedet for tekst
+    });
 });
