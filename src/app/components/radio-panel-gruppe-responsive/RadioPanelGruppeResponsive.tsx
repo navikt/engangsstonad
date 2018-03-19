332149fd1b449494247f2f75e05d8f3a9f3f934a
Erlend Vige <erlend.vige@gmail.com>
2018-03-19 11:52:47 +0100

    Improve responsiveness

--- src/app/components/radio-panel-gruppe-responsive/RadioPanelGruppeResponsive.tsx
+++ src/app/components/radio-panel-gruppe-responsive/RadioPanelGruppeResponsive.tsx
@@ -1,13 +1,31 @@
 import * as React from 'react';
-const { RadioPanelGruppe } = require('nav-frontend-skjema');
+const { SkjemaGruppe, Fieldset, RadioPanel } = require('nav-frontend-skjema');
 import { RadioPanelGruppeProps } from 'nav-frontend-skjema';
 import './radioPanelGruppeResponsive.less';
+import 'nav-frontend-skjema-style';
 
 export default class RadioPanelGruppeResponsive extends React.Component<RadioPanelGruppeProps> {
     render() {
+        const { feil, legend, checked, name, radios, onChange } = this.props;
         return (
-            <div className="radioPanelGruppeResponsive">
-                <RadioPanelGruppe {...this.props} />
+            <div className="radioPanelGruppe">
+                <Fieldset legend={legend}>
+                    <SkjemaGruppe className="radioPanelGruppe--responsive" feil={feil}>
+                            {
+                                radios.map((radio) => (
+                                    <div className="radioPanelWrapper">
+                                        <RadioPanel
+                                            checked={checked === radio.value}
+                                            name={name}
+                                            onChange={(event: any) => onChange(event, radio.value)}
+                                            key={radio.value}
+                                            {...radio}
+                                        />
+                                    </div>
+                                ))
+                            }
+                    </SkjemaGruppe>
+                </Fieldset>
             </div>
         );
     }
