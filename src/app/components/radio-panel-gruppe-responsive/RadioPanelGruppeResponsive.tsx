48c48a904c2451e4d858ae2b2bec65d5c965ec5e
Frode Hansen <frode.hansen@bekk.no>
2018-03-20 15:51:07 +0100

    Flytte key til riktig element

--- src/app/components/radio-panel-gruppe-responsive/RadioPanelGruppeResponsive.tsx
+++ src/app/components/radio-panel-gruppe-responsive/RadioPanelGruppeResponsive.tsx
@@ -4,26 +4,33 @@ import { RadioPanelGruppeProps } from 'nav-frontend-skjema';
 import './radioPanelGruppeResponsive.less';
 import 'nav-frontend-skjema-style';
 
-export default class RadioPanelGruppeResponsive extends React.Component<RadioPanelGruppeProps> {
+export default class RadioPanelGruppeResponsive extends React.Component<
+    RadioPanelGruppeProps
+> {
     render() {
         const { feil, legend, checked, name, radios, onChange } = this.props;
         return (
             <div className="radioPanelGruppe">
                 <Fieldset legend={legend}>
-                    <SkjemaGruppe className="radioPanelGruppe--responsive" feil={feil}>
-                            {
-                                radios.map((radio) => (
-                                    <div className="radioPanelWrapper">
-                                        <RadioPanel
-                                            checked={checked === radio.value}
-                                            name={name}
-                                            onChange={(event: any) => onChange(event, radio.value)}
-                                            key={radio.value}
-                                            {...radio}
-                                        />
-                                    </div>
-                                ))
-                            }
+                    <SkjemaGruppe
+                        className="radioPanelGruppe--responsive"
+                        feil={feil}
+                    >
+                        {radios.map((radio) => (
+                            <div
+                                className="radioPanelWrapper"
+                                key={radio.value}
+                            >
+                                <RadioPanel
+                                    checked={checked === radio.value}
+                                    name={name}
+                                    onChange={(event: any) =>
+                                        onChange(event, radio.value)
+                                    }
+                                    {...radio}
+                                />
+                            </div>
+                        ))}
                     </SkjemaGruppe>
                 </Fieldset>
             </div>
