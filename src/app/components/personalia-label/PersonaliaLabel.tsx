a4e1bfac1d3e81d2a63d2afcb981d255d0bf1cda
Erlend Vige <erlend.vige@gmail.com>
2018-04-05 09:57:03 +0200

    Remove icon from PersonaliaLabel

--- src/app/components/personalia-label/PersonaliaLabel.tsx
+++ src/app/components/personalia-label/PersonaliaLabel.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 const { Normaltekst, Undertittel } = require('nav-frontend-typografi');
-import CustomSVG from 'components/custom-svg/CustomSVG';
-const femaleIcon = require('assets/svg/icon_female.svg');
 
 import './personaliaLabel.less';
 
@@ -12,7 +10,6 @@ interface Props {
 
 const PersonaliaLabel: React.StatelessComponent<Props> = ({ navn, personnummer }) => (
     <div className="personaliaLabel">
-        <CustomSVG className="personaliaLabel__icon" iconRef={femaleIcon.default} size={32} />
         <div className="personaliaLabel__right">
             <Undertittel className="personaliaLabel__name">{navn}</Undertittel>
             <Normaltekst className="personaliaLabel__person">{personnummer}</Normaltekst>
