e2e701e5dd2ec541ee8e68ff28fe59dc5fcec846
Frode Hansen <frode.hansen@bekk.no>
2018-04-11 10:11:45 +0200

    Attachments update

--- src/app/components/attachment/Attachments.tsx
+++ src/app/components/attachment/Attachments.tsx
@@ -3,22 +3,24 @@ import AttachmentButton from 'components/attachment/AttachmentButton';
 import AttachmentList from 'components/attachment/AttachmentList';
 
 export interface Props {
-    vedlegg?: File[];
+    vedlegg: File[];
     onFileSelect: (files: File[]) => void;
     onFileDelete: (file: File) => void;
 }
 
-const Attachments: React.StatelessComponent<Props> = ({ vedlegg = [], onFileDelete, onFileSelect }) => (
+const Attachments: React.StatelessComponent<Props> = ({ vedlegg, onFileDelete, onFileSelect }) => (
     <div className="attachments">
         <div className="attachments__addButton">
-            <AttachmentButton id="vedlegg" onFileSelected={(files: File[]) => onFileSelect(files)} />
+            <AttachmentButton
+                id="vedlegg"
+                onFileSelected={(files: File[]) => onFileSelect(files)}
+            />
         </div>
-        {vedlegg &&
-            vedlegg.length > 0 && (
-                <div className="attachments__list">
-                    <AttachmentList vedlegg={vedlegg} onDelete={(file: File) => onFileDelete(file)} />
-                </div>
-            )}
+        {vedlegg.length > 0 && (
+            <div className="attachments__list">
+                <AttachmentList vedlegg={vedlegg} onDelete={(file: File) => onFileDelete(file)} />
+            </div>
+        )}
     </div>
 );
 
