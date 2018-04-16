82ad30fe76b3ad78d1ea618ffb7716efc1ebceec
Frode Hansen <frode.hansen@bekk.no>
2018-04-16 13:35:10 +0200

    Resette formatering av AppContainer

--- src/app/containers/AppContainer.tsx
+++ src/app/containers/AppContainer.tsx
@@ -1,12 +1,6 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import {
-    Switch,
-    Route,
-    Redirect,
-    withRouter,
-    RouteComponentProps
-} from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter, RouteComponentProps } from 'react-router-dom';
 import Spinner from 'nav-frontend-spinner';
 
 import Intro from './../connected-components/intro/Intro';
@@ -17,16 +11,9 @@ import PersonFinnesIkke from '../connected-components/feilsider/PersonFinnesIkke
 import InnsendingFeilet from '../connected-components/feilsider/InnsendingFeilet';
 
 import SøknadContainer from './SøknadContainer';
-import {
-    erMann,
-    erMyndig,
-    harPersonData
-} from 'util/validation/validationUtils';
+import { erMann, erMyndig, harPersonData } from 'util/validation/validationUtils';
 
-import {
-    apiActionCreators as api,
-    soknadActionCreators as soknad
-} from '../redux/actions';
+import { apiActionCreators as api, soknadActionCreators as soknad } from '../redux/actions';
 import { ExternalProps } from '../types';
 
 import { DispatchProps } from '../redux/types';
@@ -46,10 +33,7 @@ interface StateProps {
     søknadSendingInProgress: boolean;
 }
 
-type Props = StateProps &
-    ExternalProps &
-    DispatchProps &
-    RouteComponentProps<{}>;
+type Props = StateProps & ExternalProps & DispatchProps & RouteComponentProps<{}>;
 
 type Error = {
     personFinnes: boolean;
@@ -85,8 +69,7 @@ export class AppContainer extends React.Component<Props> {
     }
 
     redirectToLogin() {
-        window.location.href =
-            (window as any).LOGIN_URL + '?redirect=' + window.location.href;
+        window.location.href = (window as any).LOGIN_URL + '?redirect=' + window.location.href;
     }
 
     renderContent(children: React.ReactNode) {
@@ -113,41 +96,19 @@ export class AppContainer extends React.Component<Props> {
         } else if (error.innsendingFeilet === true) {
             component = InnsendingFeilet;
         }
-        return this.renderRoutes(
-            <Route path="/engangsstonad" component={component} key="error" />
-        );
+        return this.renderRoutes(<Route path="/engangsstonad" component={component} key="error" />);
     }
 
     getSøknadRoutes() {
         const { godkjentVilkar, søknadSendt } = this.props;
         const routes = [];
         if (!søknadSendt) {
-            routes.push(
-                <Route
-                    path="/engangsstonad"
-                    component={Intro}
-                    exact={true}
-                    key="intro"
-                />
-            );
+            routes.push(<Route path="/engangsstonad" component={Intro} exact={true} key="intro" />);
             if (godkjentVilkar) {
-                routes.push(
-                    <Route
-                        path="/engangsstonad/soknad"
-                        component={SøknadContainer}
-                        key="søknad"
-                    />
-                );
+                routes.push(<Route path="/engangsstonad/soknad" component={SøknadContainer} key="søknad" />);
             }
         } else {
-            routes.push(
-                <Route
-                    path="/engangsstonad"
-                    component={SøknadSendt}
-                    exact={true}
-                    key="completed"
-                />
-            );
+            routes.push(<Route path="/engangsstonad" component={SøknadSendt} exact={true} key="completed" />);
         }
         return this.renderRoutes(routes);
     }
@@ -156,25 +117,15 @@ export class AppContainer extends React.Component<Props> {
         const { person, søknadSendt, error, isLoadingPerson } = this.props;
 
         if (!person && !error && !isLoadingPerson) {
-            return this.renderContent(
-                this.getErrorRoutes({ personFinnes: false })
-            );
+            return this.renderContent(this.getErrorRoutes({ personFinnes: false }));
         }
 
         if (person) {
             const personFinnes = harPersonData(person);
             const personErMyndig = erMyndig(person);
             const personErMann = erMann(person);
-            const innsendingFeilet =
-                søknadSendt &&
-                error &&
-                error.status !== 401 &&
-                error.status >= 400;
-            const applicationStateIsValid =
-                personFinnes &&
-                personErMyndig &&
-                !personErMann &&
-                !innsendingFeilet;
+            const innsendingFeilet = søknadSendt && error && error.status !== 401 && error.status >= 400;
+            const applicationStateIsValid = personFinnes && personErMyndig && !personErMann && !innsendingFeilet;
 
             if (applicationStateIsValid) {
                 return this.renderContent(this.getSøknadRoutes());
@@ -205,6 +156,4 @@ const mapStateToProps = (state: any) => ({
     language: state.commonReducer.language
 });
 
-export default withRouter(
-    connect<StateProps, {}, {}>(mapStateToProps)(AppContainer)
-);
+export default withRouter(connect<StateProps, {}, {}>(mapStateToProps)(AppContainer));
