a3b960f2176bfce702fcde3d3f39afbb9e204368
Odd Einar Hoel <odd.einar.hoel@nav.no>
2018-04-17 11:03:13 +0200

    Add cancel-button component

---
+++ src/app/components/cancel-button/CancelButton.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { injectIntl, InjectedIntlProps } from 'react-intl';
+import { Link, Route } from 'react-router-dom';
+import Redirect from 'components/redirect/Redirect';
+import './cancelButton.less';
+import getMessage from 'util/i18n/i18nUtils';
+
+interface Props {
+    redirect: string;
+}
+
+const CancelButton: React.StatelessComponent<Props & InjectedIntlProps> = ({ intl, redirect }) => {
+    return (
+        <div className="cancelButtonWrapper">
+            <Route
+                path="/engangsstonad/soknad/cancel"
+                component={() => <Redirect loc={redirect} />}
+            />
+            <Link
+                className="cancelButton"
+                tabIndex={-1}
+                to="/engangsstonad/soknad/cancel"
+            >
+                    {getMessage(intl, 'standard.avbrytSøknad')}
+            </Link>
+        </div>
+    );
+};
+export default injectIntl(CancelButton);
