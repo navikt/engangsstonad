dcb66ea293cd3121449dd957b8a0ac05f6af9995
Odd Einar Hoel <odd.einar.hoel@nav.no>
2018-11-20 14:34:13 +0100

    Check for 413 status code for attachments that are too large

--- src/common/storage/attachment/components/AttachmentOverview.tsx
+++ src/common/storage/attachment/components/AttachmentOverview.tsx
@@ -59,7 +59,7 @@ class AttachmentOverview extends React.Component<Props, State> {
     }
 
     createErrorMessage(error: any): string {
-        if (error.response && error.response.status === 400) {
+        if (error.response && error.response.status === 413) {
             return 'vedlegg.forStort';
         }
         return 'vedlegg.feilmelding';
