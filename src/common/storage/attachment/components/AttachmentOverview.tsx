17e500dcc88351ffcea6d391cf65840a2d171a93
Odd Einar Hoel <odd.einar.hoel@nav.no>
2018-11-28 12:28:49 +0100

    Fix typo

--- src/common/storage/attachment/components/AttachmentOverview.tsx
+++ src/common/storage/attachment/components/AttachmentOverview.tsx
@@ -59,7 +59,7 @@ class AttachmentOverview extends React.Component<Props, State> {
     }
 
     createErrorMessage(error: any): string {
-        if (error && rror.response && error.response.status === 413) {
+        if (error && error.response && error.response.status === 413) {
             return 'vedlegg.forStort';
         }
         return 'vedlegg.feilmelding';
