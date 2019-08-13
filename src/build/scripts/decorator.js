d946c92b6f3362ba38e8f046c9755d812ab39338
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-08-13 16:20:36 +0200

    Use nais cluster name to determine decorator url

--- src/build/scripts/decorator.js
+++ src/build/scripts/decorator.js
@@ -3,22 +3,18 @@ const jsdom = require('jsdom');
 const request = require('request');
 
 const { JSDOM } = jsdom;
-const requestDecorator = (callback) =>
-    request(
-        `${
-            process.env.APPRES_CMS_URL
-        }/common-html/v4/navno?header=true&styles=true&scripts=true&footer=true`,
+const requestDecorator = (callback) => {
+    const baseUrl = process.env.NAIS_CLUSTER_NAME === 'dev-sbs' ? 'https://appres-q0.nav.no' : 'https://appres.nav.no';
+    return request(
+        `${baseUrl}/common-html/v4/navno?header-withmenu=true&styles=true&scripts=true&footer=true`,
         callback
     );
+};
 
 const getDecorator = () =>
     new Promise((resolve, reject) => {
         const callback = (error, response, body) => {
-            if (
-                !error &&
-                response.statusCode >= 200 &&
-                response.statusCode < 400
-            ) {
+            if (!error && response.statusCode >= 200 && response.statusCode < 400) {
                 const { document } = new JSDOM(body).window;
                 const prop = 'innerHTML';
 
