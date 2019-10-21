7dc682c816a6855a789d0b6a415bd099fa9b2710
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-10-21 11:54:33 +0200

    Revert "Ignore certificate issues for test purposes"
    This reverts commit d29c4c13a9928d30a9779508d023203819cef094.

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -1,36 +1,31 @@
 import axios from 'axios';
 import EngangsstonadSoknad from '../types/domain/EngangsstonadSoknad';
 import { AppState } from 'common/redux/types';
-import https from 'https';
-
-const agent = new https.Agent({
-    rejectUnauthorized: false
-});
 
 function getPerson() {
     const endpoint = (window as any).REST_API_URL;
-    return axios.get(`${endpoint}/personinfo`, { withCredentials: true, httpsAgent: agent });
+    return axios.get(`${endpoint}/personinfo`, {withCredentials: true});
 }
 
 function getAppState() {
-    const url = `${(<any>window).REST_API_URL}/storage`;
+    const url = `${(<any> window).REST_API_URL}/storage`;
     return axios.get(url, { withCredentials: true });
 }
 
 function saveAppState(state: AppState) {
-    const url = `${(<any>window).REST_API_URL}/storage`;
+    const url = `${(<any> window).REST_API_URL}/storage`;
     return axios.post(url, state, { withCredentials: true });
 }
 
 function sendSoknad(soknad: EngangsstonadSoknad) {
-    const config = {
+    const config  = {
         withCredentials: true,
         headers: {
             'content-type': 'application/json;'
         }
     };
-
-    const url = `${(<any>window).REST_API_URL}/soknad`;
+    
+    const url = `${(<any> window).REST_API_URL}/soknad`;
     return axios.post(url, soknad, config);
 }
 
