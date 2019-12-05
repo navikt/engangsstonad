bc90ab2678c8fa6df9acc47b8b6ee7f40e0ea092
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-12-05 13:30:28 +0100

    Add test for text keys & add missing keys

---
+++ src/app/intl/__tests__/intl.test.ts
@@ -0,0 +1,22 @@
+const nb = require('../nb_NO.json');
+const nn = require('../nn_NO.json');
+const en = require('../en_US.json');
+
+const _ = require('lodash');
+
+describe('intl tests', () => {
+    it('Language files should have exactly the same keys', () => {
+        const keysBokmål = Object.keys(nb);
+        const keysNynorsk = Object.keys(nn);
+        const keysEnglish = Object.keys(en);
+
+        expect(keysBokmål.sort()).toMatchObject(keysNynorsk.sort());
+        expect(keysBokmål.sort()).toMatchObject(keysEnglish.sort());
+
+        expect(keysNynorsk.sort()).toMatchObject(keysBokmål.sort());
+        expect(keysNynorsk.sort()).toMatchObject(keysEnglish.sort());
+
+        expect(keysEnglish.sort()).toMatchObject(keysBokmål.sort());
+        expect(keysEnglish.sort()).toMatchObject(keysEnglish.sort());
+    });
+});
