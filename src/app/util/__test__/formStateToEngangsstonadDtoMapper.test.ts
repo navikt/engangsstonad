0b296cf32f4bc2932b7a3d620e136dd80d07724b
Odd Einar Hoel <odd.einar.hoel@nav.no>
2020-03-31 20:28:22 +0200

    Remove rendundant tests

--- src/app/util/__test__/formStateToEngangsstonadDtoMapper.test.ts
+++ src/app/util/__test__/formStateToEngangsstonadDtoMapper.test.ts
@@ -1,7 +1,6 @@
-import { mapFormStateToEngangsstonadDto } from 'util/formStateToEngangsttonadDtoMapper';
+import { mapFormStateToEngangsstonadDto } from '../formStateToEngangsttonadDtoMapper';
 import { Language } from 'intl/IntlProvider';
 import { FormProps } from 'app/engangsstonad/FormProps';
-import { Attachment } from 'common/storage/attachment/types/Attachment';
 import { Utenlandsopphold } from 'app/types/domain/InformasjonOmUtenlandsopphold';
 
 describe('mapFormStateToEngangsstonadDto', () => {
@@ -13,7 +12,7 @@ describe('mapFormStateToEngangsstonadDto', () => {
             fodselsdato: '2020-01-01',
             termindato: '2020-01-01',
             // tslint:disable-next-line: no-object-literal-type-assertion
-            terminberkreftelse: [{} as Attachment],
+            terminberkreftelse: [],
             terminbekreftelseDato: '2020-01-01',
             navn: 'jens',
             kanIkkeOppgis: false,
@@ -29,35 +28,19 @@ describe('mapFormStateToEngangsstonadDto', () => {
         };
     });
 
-    it('skal mappe ufødt barn riktig', () => {
-        const barn = mapFormStateToEngangsstonadDto(formValues, Language.BOKMÅL).barn;
-        expect(barn).toHaveProperty(['erBarnetFødt', 'antallBarn', 'fødselsdatoer']);
-        expect(Object.keys(barn)).toEqual(3);
-    });
-
     it('skal mappe født barn riktig', () => {
         const barn = mapFormStateToEngangsstonadDto({ ...formValues, erFødt: true }, Language.BOKMÅL).barn;
-        expect(barn).toHaveProperty([
-            'erBarnetFødt',
-            'antallBarn',
-            'termindato',
-            'terminbebkreftelse',
-            'terminbekreftelseDato'
-        ]);
-        expect(Object.keys(barn)).toEqual(5);
+        expect(barn).toMatchObject({ erBarnetFødt: true, antallBarn: 1, fødselsdatoer: ['2020-01-01'] });
     });
 
-    it('skal mappe annen forelder riktig hvis annen foredlder ikke kan oppgis', () => {
-        const annenForelder = mapFormStateToEngangsstonadDto({ ...formValues, kanIkkeOppgis: true }, Language.BOKMÅL)
-            .annenForelder;
-        expect(annenForelder).toHaveProperty(['kanIkkeOppgis']);
-        expect(Object.keys(annenForelder)).toEqual(1);
-    });
-
-    it('skal mappe annen forelder riktig hvis annen forelder er kjent', () => {
-        const annenForelder = mapFormStateToEngangsstonadDto({ ...formValues, kanIkkeOppgis: false }, Language.BOKMÅL)
-            .annenForelder;
-        expect(annenForelder).toHaveProperty(['kanIkkeOppgis', 'navn', 'fnr', 'utenlandskFnr']);
-        expect(Object.keys(annenForelder)).toEqual(1);
+    it('skal mappe ufødt barn riktig', () => {
+        const barn = mapFormStateToEngangsstonadDto({ ...formValues, erFødt: false }, Language.BOKMÅL).barn;
+        expect(barn).toMatchObject({
+            erBarnetFødt: false,
+            antallBarn: 1,
+            termindato: '2020-01-01',
+            terminbekreftelse: [],
+            terminbekreftelseDato: '2020-01-01'
+        });
     });
 });
